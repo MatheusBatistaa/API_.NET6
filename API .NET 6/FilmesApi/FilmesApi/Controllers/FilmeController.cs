786e9651b26cbc3837d51c34a3c6aedd5cf5c00e
MatheusBatistaa <86001281+MatheusBatistaa@users.noreply.github.com>
2023-05-02 09:51:33 -0300

    Projeto API .NET 6
    Projeto sendo feito pela Alura.

---
+++ API .NET 6/FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,25 @@
+﻿using FilmesApi.Molds;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+
+        private static List<Filme> filmes = new List<Filme>();
+
+
+        [HttpPost] //Estamos postando um informação nova
+        public void AdicionaFilme( [FromBody] Filme filme) //From body - Parametro 
+        {
+                        
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+
+        }
+
+    }
+}
---
+++ API .NET 6/FilmesApi/FilmesApi/Molds/Filme.cs
@@ -0,0 +1,12 @@
+﻿namespace FilmesApi.Molds;
+
+public class Filme
+{
+    public string Titulo { get; set; }
+   
+    public string Genero { get; set; }
+
+    public string Duracao { get; set;}
+
+
+}
